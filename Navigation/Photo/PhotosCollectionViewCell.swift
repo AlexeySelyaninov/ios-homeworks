f2db992c389f0864554479a46067ebdf276fe462
AlexeySelyaninov <student_selyaninov@mail.ru>
2022-07-06 21:20:48 +0300

    add home task all

---
+++ Navigation/Photo/PhotosCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Navigation
+//
+//  Created by Алексей Селянинов on 06.07.2022.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
